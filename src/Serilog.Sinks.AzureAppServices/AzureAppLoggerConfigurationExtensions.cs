63ebb56ad4b9f32e4bd50e7075d677f688ff9d60
Gabriel Bider <1554615+silkfire@users.noreply.github.com>
2022-09-06 16:17:26 +0200

    * Rename project to AzureAppServices * Add metadata * Bump version to 4

--- src/Serilog.Sinks.AzureAppServices/AzureAppLoggerConfigurationExtensions.cs
+++ src/Serilog.Sinks.AzureAppServices/AzureAppLoggerConfigurationExtensions.cs
@@ -1,4 +1,4 @@
-// Copyright 2013-2018 Serilog Contributors
+// Copyright 2013-2022 Serilog Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,26 +12,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using Serilog.Configuration;
-using Serilog.Core;
-using Serilog.Events;
-using Serilog.Formatting.Display;
-using Serilog.Sinks.AzureApp;
-using Serilog.Formatting;
-using Serilog.Formatting.Json;
+
 
 namespace Serilog
 {
+    using Sinks.AzureAppServices;
+
+    using Configuration;
+    using Core;
+    using Events;
+    using Formatting.Display;
+    using Formatting;
+    using Formatting.Json;
+
+    using System;
+
     /// <summary>
-    /// Adds the WriteTo.AzureApp() extension method to <see cref="LoggerConfiguration"/>.
+    /// Adds the <c>Serilog.WriteTo.AzureAppServices()</c> extension method to <see cref="LoggerConfiguration"/>.
     /// </summary>
     public static class TraceLoggerConfigurationExtensions
     {
         private const string DefaultOutputTemplate = "{Message}{NewLine}{Exception}";
 
         /// <summary>
-        /// Write log events to the Azure Diagnostic Application Log/>.
+        /// Write log events to the Azure Web Apps diagnostics log.
         /// </summary>
         /// <param name="sinkConfiguration">Logger sink configuration.</param>
         /// <param name="restrictedToMinimumLevel">The minimum level for
@@ -40,9 +44,9 @@ namespace Serilog
         /// to be changed at runtime.</param>
         /// <param name="outputTemplate">A message template describing the format used to write to the sink.
         /// the default is "{Message}{NewLine}{Exception}".</param>
-        /// <param name="formatProvider">Supplies culture-specific formatting information, or null.</param>
+        /// <param name="formatProvider">Supplies culture-specific formatting information, or <see langword="null"/>.</param>
         /// <returns>Configuration object allowing method chaining.</returns>
-        public static LoggerConfiguration AzureApp(
+        public static LoggerConfiguration AzureAppServices(
             this LoggerSinkConfiguration sinkConfiguration,
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
             string outputTemplate = DefaultOutputTemplate,
@@ -51,12 +55,14 @@ namespace Serilog
         {
             if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
             if (outputTemplate == null) throw new ArgumentNullException(nameof(outputTemplate));
+
             var formatter = new MessageTemplateTextFormatter(outputTemplate, formatProvider);
-            return AzureApp(sinkConfiguration, formatter, restrictedToMinimumLevel, levelSwitch);
+
+            return AzureAppServices(sinkConfiguration, formatter, restrictedToMinimumLevel, levelSwitch);
         }
 
         /// <summary>
-        /// Write log events to the Azure Diagnostic Application Log/>.
+        /// Write log events to the Azure Web Apps diagnostics log.
         /// </summary>
         /// <param name="sinkConfiguration">Logger sink configuration.</param>
         /// <param name="restrictedToMinimumLevel">The minimum level for
@@ -67,7 +73,7 @@ namespace Serilog
         /// e.g. <see cref="JsonFormatter"/> to produce JSON output. To customize the text layout only, use the
         /// overload that accepts an output template instead.</param>
         /// <returns>Configuration object allowing method chaining.</returns>
-        public static LoggerConfiguration AzureApp(
+        public static LoggerConfiguration AzureAppServices(
             this LoggerSinkConfiguration sinkConfiguration,
             ITextFormatter formatter,
             LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum,
@@ -75,7 +81,8 @@ namespace Serilog
         {
             if (sinkConfiguration == null) throw new ArgumentNullException(nameof(sinkConfiguration));
             if (formatter == null) throw new ArgumentNullException(nameof(formatter));
-            return sinkConfiguration.Sink(new AzureAppSink(formatter), restrictedToMinimumLevel, levelSwitch);
+
+            return sinkConfiguration.Sink(new AzureAppServicesSink(formatter), restrictedToMinimumLevel, levelSwitch);
         }
     }
 }
--- src/Serilog.Sinks.AzureAppServices/Sinks/AzureAppServices/AzureAppServicesSink.cs
+++ src/Serilog.Sinks.AzureAppServices/Sinks/AzureAppServices/AzureAppServicesSink.cs
@@ -1,4 +1,4 @@
-// Copyright 2013-2018 Serilog Contributors
+// Copyright 2013-2022 Serilog Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,34 +12,35 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Microsoft.Extensions.Logging;
-using Serilog.Core;
-using Serilog.Events;
-using Serilog.Formatting;
-
-using System;
-using System.Collections.Concurrent;
-using System.IO;
-
-namespace Serilog.Sinks.AzureApp
+namespace Serilog.Sinks.AzureAppServices
 {
-    internal class AzureAppSink : ILogEventSink
+    using Core;
+    using Events;
+    using Formatting;
+
+    using Microsoft.Extensions.Logging;
+
+    using System;
+    using System.Collections.Concurrent;
+    using System.IO;
+
+    internal class AzureAppServicesSink : ILogEventSink
     {
         /// <summary>
-        /// Our very own Microsoft.Extensions.LoggerFactory, this is where we'll send Serilog events so that Azure can pick up the logs.
+        /// Our very own <see cref="LoggerFactory"/>, this is where we'll send Serilog events so that Azure can pick up the logs.
         /// We expect that Serilog has replaced this in the app's services.
         /// </summary>
         private static ILoggerFactory CoreLoggerFactory { get; } = LoggerFactory.Create(builder => builder.AddAzureWebAppDiagnostics());
 
         /// <summary>
-        /// The Microsoft.Extensions.LoggerFactory implementation of CreateLogger(string category) uses lock(_sync) before looking in its dictionary.
-        /// We'll use our own ConcurrentDictionary for performance, since we lookup the category on every log write.
+        /// The <see cref="LoggerFactory"/> implementation of <see cref="LoggerFactory.CreateLogger"/> uses <c>lock(_sync)</c> before looking in its dictionary.
+        /// We'll use our own <see cref="ConcurrentDictionary{TKey,TValue}"/> for performance, since we lookup the category on every log write.
         /// </summary>
-        private readonly ConcurrentDictionary<string, Microsoft.Extensions.Logging.ILogger> loggerCategories = new ConcurrentDictionary<string, Microsoft.Extensions.Logging.ILogger>();
+        private readonly ConcurrentDictionary<string, ILogger> _loggerCategories = new ConcurrentDictionary<string, ILogger>();
 
         private readonly ITextFormatter _textFormatter;
 
-        public AzureAppSink(ITextFormatter textFormatter)
+        public AzureAppServicesSink(ITextFormatter textFormatter)
         {
             _textFormatter = textFormatter ?? throw new ArgumentNullException(nameof(textFormatter));
         }
@@ -53,7 +54,7 @@ namespace Serilog.Sinks.AzureApp
             var text = sr.ToString().Trim();
 
             var category = logEvent.Properties.TryGetValue("SourceContext", out var value) ? value.ToString() : "";
-            var logger = loggerCategories.GetOrAdd(category, s => CoreLoggerFactory.CreateLogger(s));
+            var logger = _loggerCategories.GetOrAdd(category, s => CoreLoggerFactory.CreateLogger(s));
 
             switch (logEvent.Level)
             {
